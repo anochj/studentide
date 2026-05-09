c2f8b9a7ea55c97bf6a6db79ec28c0c28afb5393
Anoch <anoch@Anochs-MacBook-Air-4.local>
2026-05-09 00:59:28 -0400

    fix: cors issue

--- apps/web/lib/auth-client.ts
+++ apps/web/lib/auth-client.ts
@@ -1,10 +1,9 @@
 import { stripeClient } from "@better-auth/stripe/client";
 import { usernameClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
-import { env } from "@/lib/env.client";
 
 export const authClient = createAuthClient({
-  baseURL: env.BETTER_AUTH_URL,
+  basePath: "/api/auth",
   plugins: [
     usernameClient(),
     stripeClient({
